6fe6641a1e809c5d123bafe7955dc53108d211b7
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-05-07 20:55:35 +0200

    Remove test not present in mealie-next

--- tests/unit_tests/core/test_security.py
+++ tests/unit_tests/core/test_security.py
@@ -6,37 +6,10 @@ from pytest import MonkeyPatch
 from mealie.core import security
 from mealie.core.config import get_app_settings
 from mealie.core.dependencies import validate_file_token
-from mealie.repos.all_repositories import get_repositories
-from mealie.core.security.hasher import FakeHasher, PasslibHasher, get_hasher
 from mealie.db.db_setup import session_context
 from mealie.db.models.users.users import AuthMethod
 from mealie.schema.user.user import PrivateUser
 from tests.utils import random_string
-
-# ===============================================
-# SECURITY
-
-
-def test_get_hasher(monkeypatch: MonkeyPatch):
-    hasher = get_hasher()
-
-    assert isinstance(hasher, FakeHasher)
-
-    monkeypatch.setenv("TESTING", "0")
-
-    get_hasher.cache_clear()
-    get_app_settings.cache_clear()
-
-    hasher = get_hasher()
-
-    assert isinstance(hasher, PasslibHasher)
-
-    get_app_settings.cache_clear()
-    get_hasher.cache_clear()
-
-
-# ===============================================
-# LDAP
 
 
 class LdapConnMock:
